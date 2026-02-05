1ddcd952337d40620fca459c06054cf77c0cd2b3
Leo Pearson <wgls0770@leeds.ac.uk>
2026-02-05 10:23:21 +0000

    Finish session 1

--- session_1/1_country/task1.sql
+++ session_1/1_country/task1.sql
@@ -12,4 +12,13 @@
 
 -- write your sql code here
 
-SELECT * from countries;
+SELECT * FROM countries;
+SELECT Country, Population FROM countries;
+SELECT Country FROM countries WHERE Continent='Europe';
+SELECT * FROM countries WHERE Population>100000000;
+SELECT Country FROM countries WHERE Continent='Asia' AND GDPPerCapita>20000;
+SELECT Country FROM countries WHERE LiteracyPercent<70;
+SELECT Country FROM countries WHERE CoastlineRatio=0;
+SELECT Country FROM countries WHERE Country LIKE 'A%';
+SELECT Country FROM countries WHERE Country LIKE '%United%';
+SELECT Country FROM countries WHERE Birthrate>Deathrate AND Population>50000000;
